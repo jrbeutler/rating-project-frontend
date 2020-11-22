e20697264cf7add9d300b07b8d43e76cc3531422
Eli Sokeland <etsokeland@bsu.edu>
2020-11-22 04:13:11 -0500

    Got basic flex styling for profile section

--- src/pages/Account/Account.tsx
+++ src/pages/Account/Account.tsx
@@ -2,8 +2,32 @@ import React, { useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import ProfilePlaceholder from '../../assets/ProfilePlaceholder.svg';
 import { AuthContext, UserContext } from '../../App';
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    accountPage: {
+      backgroundColor: '#85CAB0',
+      width: '100%',
+      height: '100vh',
+    },
+    profile: {
+      display: 'flex',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      width: '100%',
+      justifyContent: 'space-evenly',
+    },
+    profileImage: {
+      width: '20rem',
+      height: 'auto',
+    },
+  }),
+);
 
 const Account: React.FC = () => {
+  const classes = useStyles();
   const sessionContext = useContext(AuthContext);
   const userContext = useContext(UserContext);
   const history = useHistory();
@@ -17,13 +41,13 @@ const Account: React.FC = () => {
   console.log(userContext.currentUser);
 
   return (
-    <section>
-      <section>
-        <img src={ProfilePlaceholder} alt='Profile Image' title='Profile Image' />
+    <section className={classes.accountPage}>
+      <section className={classes.profile}>
+        <img src={ProfilePlaceholder} alt='Profile Image' title='Profile Image' className={classes.profileImage} />
         <article>
-          <h1>{userContext.currentUser.firstname} {userContext.currentUser.lastname}</h1>
-          <h2>{userContext.currentUser.role}</h2>
-          <p>Overall Rating:</p>
+          <Typography variant='h1'>{userContext.currentUser.firstname} {userContext.currentUser.lastname}</Typography>
+          <Typography variant='h2'>{userContext.currentUser.role}</Typography>
+          <Typography>Overall Rating:</Typography>
         </article>
       </section>
     </section>
