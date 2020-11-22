3f453a6132782f869a876074110da7f5cb0625a7
Recon Monkee <joshuajohannsen@gmail.com>
2020-11-22 04:09:52 -0500

    Setting up Rating Page

--- src/pages/Rate/Rate.tsx
+++ src/pages/Rate/Rate.tsx
@@ -2,11 +2,23 @@ import React, { useContext, useEffect } from "react";
 import { AuthContext, UserContext } from "../../App";
 import { useHistory } from "react-router-dom";
 import { FormLabel } from "@material-ui/core";
+import Select from '@material-ui/core/Select';
 
 const Rate: React.FC = () => {
   const sessionContext = useContext(AuthContext);
   const userContext = useContext(UserContext);
   const history = useHistory();
+  const [state, setState] = React.useState({
+    reviewedID: "",
+    Category: "",
+  });
+  const handleChange = (event: React.ChangeEvent<{ name?: string; value: unknown }>) => {
+    const name = event.target.name as keyof typeof state;
+    setState({
+      ...state,
+      [name]: event.target.value,
+    });
+  };
 
   useEffect(() => {
     if (sessionContext.loginSession === '') {
@@ -14,13 +26,53 @@ const Rate: React.FC = () => {
     }
   });
 
+
+
   return (
     <section>
       <h1>Rate an Apprentice</h1>
       <form>
         <FormLabel required>Apprentice</FormLabel>
-
       </form>
+        <form>
+        <Select
+          native
+          required
+          value={state.reviewedID}
+          onChange={handleChange}
+          inputProps={{
+            name: "Apprentice",
+            id: 'Apprentice',
+          }}
+        >
+          <option value="">Select an Apprentice:</option>
+          <option value={'Bart Simpson'}>Bart Simpson</option>
+        </Select>
+      </form>
+      <form>
+        <FormLabel required>Category</FormLabel>
+      </form>
+        <form>
+        <Select
+          native
+          required
+          value={state.Category}
+          onChange={handleChange}
+          inputProps={{
+            name: "Category",
+            id: 'Category',
+          }}
+        >
+          <option value="">Select A Category:</option>
+          <option value={'FRONTEND'}>FRONTEND</option>
+          <option value={"BACKEND"}>BACKEND</option>
+        </Select>
+      </form>
+      <form>
+        <textarea>
+        </textarea>
+      </form>
+      <button>Submit</button>
     </section>
   );
 }
--- src/utils/Requests.ts
+++ src/utils/Requests.ts
@@ -25,6 +25,65 @@ export default class Requests {
     return promise;
   }
 
+  static rate = async (reviewerID: string, reviewedID: string, category: string, rating: number, notes: string) =>{
+    const promise = await axios({
+      url: 'http://localhost:3000/graphql',
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: {
+        query: `
+        mutation {
+          createRating(
+            data: {
+              reviewedID: "${reviewedID}"
+              reviewerID: "${reviewerID}"
+              category: "${category}"
+              rating: "${rating}"
+              notes: "${notes}"
+            }
+          ) {
+            id
+            reviewedID
+            reviewerID
+            category
+            rating
+            notes
+          }
+        }`
+      }
+    });
+    return promise
+  }
+
+  static getAllUsers = (sessionToken: string) => {
+    axios({
+      url: 'http://localhost:3000/graphql',
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${sessionToken}`
+      },
+      data: {
+        query:`
+        query{
+          allUsers {
+            id
+            email
+            firstname
+            lastname
+          }
+        }
+        `
+      }
+    }).then((result) => {
+      return(result);
+    }).catch((e) => {
+      return(e.message);
+    });
+  }
+
   static getCurrentUser = (sessionToken: string) => {
     axios({
       url: 'http://localhost:3000/graphql',
