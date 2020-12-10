fccec1ebed46dd22f253d3c67378427e30c60645
Eli Sokeland <etsokeland@bsu.edu>
2020-12-09 23:55:56 -0600

    Changed Up Request Function Structure
    Broken down by file into respective functionality.

---
+++ src/utils/requests/Rating.ts
@@ -0,0 +1,92 @@
+import config from "../../config";
+import axios from "axios";
+
+// This file contains all network requests related to ratings.
+
+export async function rate(
+  sessionToken: string,
+  reviewerID: string,
+  reviewedID: string,
+  categoryID: string,
+  rating: number,
+  notes: string) {
+  const response = await fetch(config.apiURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${sessionToken}`
+    },
+    body: JSON.stringify({
+      query: `
+        mutation {
+          createRating(
+            data: {
+              reviewedID: "${reviewedID}"
+              reviewerID: "${reviewerID}"
+              categoryID: "${categoryID}"
+              rating: ${rating}
+              notes: "${notes}"
+            }
+          ) {
+            id
+            reviewedID
+            reviewerID
+            categoryID
+            rating
+            notes
+          }
+        }`
+    }),
+  });
+  return await response.json();
+}
+
+export async function getUserRatings(sessionToken: string, userID: string) {
+  const response = await fetch(config.apiURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${sessionToken.toString()}`
+    },
+    body: JSON.stringify({
+      query: `
+        query {
+          userRatings(reviewedID: "${userID}") {
+            id,
+            reviewedID,
+            reviewerID,
+            rating,
+            categoryID,
+            notes
+          }
+        }
+        `
+    }),
+  });
+  return await response.json();
+}
+
+export async function getRatingsCreated(sessionToken: string, userID: string) {
+  const response = await fetch(config.apiURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${sessionToken.toString()}`
+    },
+    body: JSON.stringify({
+      query: `
+        query {
+          userReviewedRatings(reviewerID: "${userID}") {
+            id,
+            reviewedID,
+            reviewerID,
+            rating,
+            categoryID,
+            notes
+          }
+        }
+        `
+    }),
+  });
+  return await response.json();
+}
---
+++ src/utils/requests/User.ts
@@ -0,0 +1,98 @@
+import config from "../../config";
+import axios from "axios";
+
+// This file contains all network requests related to the user.
+
+export async function login(email: string, password: string) {
+  const response = await fetch(config.apiURL,{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: `mutation {
+            login(data: {email: "${email}", password: "${password}"}) {
+              accessToken
+              user {
+                id
+                email
+                firstname
+                lastname
+                role
+              }
+            }
+          }`
+    }),
+  });
+  return await response.json();
+}
+
+export async function refreshToken(sessionToken: string) {
+  const response = await fetch(config.apiURL,{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: `
+        mutation {
+          refreshToken(
+            token: "${sessionToken}"
+          ) {
+            accessToken
+            refreshToken
+          }
+        }`
+    }),
+  });
+  return await response.json();
+}
+
+export async function getUserByID(sessionToken: string, userID: string) {
+  const response = await fetch(config.apiURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${sessionToken.toString()}`
+    },
+    body: JSON.stringify({
+      query: `
+        query {
+          userByID(userID: "${userID}") {
+            id,
+            firstname,
+            lastname,
+            email,
+            role
+          }
+        }
+        `
+    }),
+  });
+  return await response.json();
+}
+
+async function getCurrentUser(sessionToken: string) {
+  const response = await fetch(config.apiURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${sessionToken}`
+    },
+    body: JSON.stringify({
+      query: `
+        query {
+          me {
+            id,
+            email,
+            firstname,
+            lastname,
+            role
+          }
+        }
+        `
+    }),
+  });
+  return await response.json();
+}
+
