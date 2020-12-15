4193fd0e9b511e370f93f7cb8087d444cecfb573
Eli Sokeland <elisokeland@yahoo.com>
2020-12-14 20:35:12 -0600

    Revert "Add css and png file. The login function should work."

--- src/components/AppPreviewPanel/AppPreviewPanel.tsx
+++ src/components/AppPreviewPanel/AppPreviewPanel.tsx
@@ -4,7 +4,7 @@ import LandingPage from '../../assets/LandingPage.png';
 const AppPreviewPanel: React.FC = () => {
   return (
     <section>
-      {/*<img src={LandingPage} alt='Mobile Landing Page' title='Mobile Landing Page' />*/}
+      <img src={LandingPage} alt='Mobile Landing Page' title='Mobile Landing Page' />
     </section>
   )
 };
